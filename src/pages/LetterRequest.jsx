d26da3d5038e1c57d5f6d246a74e048e1d4a5ec2
HelmiITK <helmi220602@gmail.com>
2024-04-06 01:28:46 +0800

    done permohonan surat responsive

--- src/pages/LetterRequest.jsx
+++ src/pages/LetterRequest.jsx
@@ -7,6 +7,11 @@ import { TbCircleNumber1, TbCircleNumber2, TbCircleNumber3 } from "react-icons/t
 import jenisSurat from "../assets/morphis-reviewing-resumes-of-candidates.png"
 import { Link } from "react-router-dom";
 import { useRef } from "react";
+import { IoMdArrowRoundBack } from "react-icons/io";
+
+"use client";
+
+import { Carousel } from "flowbite-react";
 
 const LetterRequest = () => {
    const linkRef = useRef(null);
@@ -18,11 +23,12 @@ const LetterRequest = () => {
          behavior: "smooth",
       });
    };
+
    return (
       <>
          <Navbar />
-         {/* background */}
-         <div className="container mx-auto bg-gradient-to-tr from-orange-500 via-orange-300 to-orange-700 w-full pb-10" ref={linkRef}>
+         {/* background serta mode web dan tablet */}
+         <div className="hidden md:block container mx-auto bg-gradient-to-tr from-orange-500 via-orange-300 to-orange-700 w-full pb-10" ref={linkRef}>
             <div className="pt-28 flex flex-col items-center gap-10 justify-center">
                {/* card step by step */}
                <div className="border-none bg-white drop-shadow-2xl bg-opacity-50 py-8 px-12  w-[80%] rounded-2xl flex justify-center gap-6 flex-col mx-10">
@@ -69,6 +75,58 @@ const LetterRequest = () => {
                         <h2 className="text-lg font-medium w-[70%] text-center">Surat Permohonan</h2>
                      </Link>
                   </div>
+               </div>
+            </div>
+         </div>
+
+         {/* mode hp */}
+         <div className="container mx-auto pt-28 md:hidden ">
+            {/* button back to home */}
+            <Link as={Link} to={"/"} className="flex items-center gap-2 mx-4 mb-4">
+               <IoMdArrowRoundBack className="w-5 h-5" />
+               <p className="underline text-sm font-medium">Kembali</p>
+            </Link>
+            {/* carousel card step by step */}
+            <div className="flex flex-col gap-4 mx-4">
+               <h1 className="text-center font-semibold text-2xl text-orange-400 underline tracking-wider">
+                  Alur Pelayanan
+               </h1>
+               <div className="h-[400px] sm:h-64 xl:h-80 2xl:h-96 drop-shadow-2xl">
+                  <Carousel slideInterval={3000} className="border-2 border-slate-300 rounded-2xl p-6 shadow-xl">
+                     <div className="flex flex-col pb-16 justify-center items-center text-center">
+                        <img src={step1} alt="..." className="w-72 h-72" />
+                        <p className="w-[70%]">Pemohon mengisi formulir surat yang dibutuhkan</p>
+                     </div>
+                     <div className="flex flex-col pb-16 justify-center items-center text-center">
+                        <img src={step2} alt="..." className="w-72 h-72" />
+                        <p className="w-[70%]">Pengurus RT memproses  permohonan</p>
+                     </div>
+                     <div className="flex flex-col pb-16 justify-center items-center text-center">
+                        <img src={step3} alt="..." className="w-72 h-72" />
+                        <p className="w-[70%]">Pemohon akan dihubungi untuk mengambil surat</p>
+                     </div>
+                  </Carousel>
+               </div>
+            </div>
+            {/* card pilih surat */}
+            <div className="my-14 mx-4 flex flex-col gap-6">
+               <h1 className="text-orange-400 font-semibold text-2xl text-center underline tracking-wider">Jenis Surat</h1>
+               <div className=" grid grid-cols-2 gap-5 justify-center items-center text-center">
+                  {/* surat 1 */}
+                  <Link as={Link} to={"/"} className="border-2 border-orange-400 rounded-xl p-4 shadow-lg shadow-orange-200">
+                     <img src={jenisSurat} alt="surat 1" />
+                     <p>Surat Pengantar</p>
+                  </Link>
+                  {/* surat 2 */}
+                  <Link as={Link} to={"/"} className="border-2 border-orange-400 rounded-xl p-4 shadow-lg shadow-orange-200">
+                     <img src={jenisSurat} alt="surat 1" />
+                     <p>Surat Keterangan</p>
+                  </Link>
+                  {/* surat 3 */}
+                  <Link as={Link} to={"/"} className="border-2 border-orange-400 rounded-xl p-4 shadow-lg shadow-orange-200">
+                     <img src={jenisSurat} alt="surat 1" />
+                     <p>Surat Permohonan</p>
+                  </Link>
                </div>
             </div>
          </div>
